6ce838381d5022e0a1a1343688689de5574d594c
hitesh11364 <hiteshkumar11364@gmail.com>
2025-04-17 10:33:04 +0530

    Setup Firebase config with environment variables

--- src/Firebase.tsx
+++ src/Firebase.tsx
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.tsx
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAkcDE80RwUeMbG1v9pmvO2HpVkeRnUp64",
-    authDomain: "portfolio-87d30.firebaseapp.com",
-    projectId: "portfolio-87d30",
-    storageBucket: "portfolio-87d30.firebasestorage.app",
-    messagingSenderId: "71624182220",
-    appId: "1:71624182220:web:6a4d8d4233cc142cb53569",
-    measurementId: "G-J7MRGC5NJH"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
